04736105764a0de480ef3b4f304e63d4b3fac9e1
guojianzhe <874375365@qq.com>
2019-03-19 09:09:46 +0800

    3.19

---
+++ crm_demo/src/com/heima/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.heima.service;
+
+import java.util.List;
+
+import com.heima.domain.Customer;
+
+public interface CustomerService {
+
+	List<Customer> findAll();
+
+}
---
+++ crm_demo/src/com/heima/serviceImpl/CustomerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.heima.serviceImpl;
+
+import java.util.List;
+
+import com.heima.dao.CustomerDao;
+import com.heima.daoImpl.CustomerDaoImpl;
+import com.heima.domain.Customer;
+import com.heima.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public List<Customer> findAll() {
+		
+		CustomerDao customerDao =  new CustomerDaoImpl();
+		List<Customer> list = customerDao.findAll();
+		
+		return list;
+	}
+
+}
